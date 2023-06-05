baf5088f9cddd4925a8453bdc0c0d871af9f33fd
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-06-05 20:13:34 +0700

    update direct url middleware

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
   const url = request.url;
   const baseUrl =
     process.env.NODE_ENV === "production"
-      ? "https://web-sociagram.vercel.app/"
+      ? "https://sociagram.vercel.app/"
       : "http://localhost:3001/";
   if (url.includes("/signin") || url.includes("/signup")) {
     if (accessToken?.value) {
