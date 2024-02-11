d7afa6834a9e86c8011ef83db96790dba616a6ae
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2024-02-12 00:01:39 +0700

    update api from railway now using cycling

--- config/api.ts
+++ config/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://gold-friendly-tortoise.cyclic.app/api/"
+    ? "https://gold-friendly-tortoise.cyclic.app/api"
     : "http://localhost:3002/api";
 
 export const api = axios.create({
